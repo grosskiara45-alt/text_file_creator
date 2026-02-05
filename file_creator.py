3b2650a55392849b815e9518895fb7c1009fabe1
Kiara <grosskiara45@gmail.com>
2026-02-05 10:16:41 -0500

    Python code push with color line added

--- file_creator.py
+++ file_creator.py
@@ -2,3 +2,5 @@ name = input("What's your name? \n")
 color = input("What's your favorite color? \n")
 with open('example.txt', 'w') as file:
     file.write(f"{name} created this file")
+with open('example.txt', 'w') as file:
+    file.write(f"{color} created this file")
